b913e693ed43c59e3898a2244de321d95a281fbd
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-09 16:49:05 +1000

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = () => {
+  let result =  2000 / 100
+  return result
+}
+
+const square = (x) => {
+    let result = x * x
+    return result
+}
+const add = (a , b) => {
+    let result = a + b
+    return result
+}
